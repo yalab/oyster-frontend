ce6b5a867b2af86a3a00d0bd8a252fda77764051
yalab <rudeboyjet@gmail.com>
2021-01-27 22:39:46 +0900

    First step

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 const App: React.FC<{ compiler: string, framework: string }> = (props) => {
+  const position : L.LatLngTuple = [51.505, -0.09]
   return (
-    <div>
-      <div>{props.compiler}</div>
-      <div>{props.framework}</div>
-    </div>
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 }
 
